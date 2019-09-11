c9bd2262bab5d3e64a869c65fc55eaf03d18d4f8
Александр Рыбин <alex_rybin@playrix.com>
2019-09-11 16:53:10 +0300

    Add function to solve quiz to BasePage Add product page tests

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
+
+
+class ProductPageLocators:
+    ADD_TO_CART_BUTTON = (By.CLASS_NAME, 'btn-add-to-basket')
+    ADDED_TO_CART_MESSAGE = (By.CSS_SELECTOR, '.alert-success:nth-child(1)')
+    ADDED_TO_CART_PRODUCT = (By.CSS_SELECTOR, '.alert-success:nth-child(1) strong')
+    CART_TOTAL_MESSAGE = (By.CLASS_NAME, 'alert-info')
+    CART_TOTAL_MESSAGE_PRICE = (By.CSS_SELECTOR, '.alert-info strong')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main > h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main > .price_color')
---
+++ pages/product_page.py
@@ -0,0 +1,30 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_cart_click(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON).click()
+
+    def should_be_message_added_to_cart(self):
+        assert self.is_element_present(*ProductPageLocators.ADDED_TO_CART_MESSAGE), (
+            'No \"added to cart\" message'
+        )
+
+    def should_be_same_product_name(self):
+        product_name_header = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_name_message = self.browser.find_element(
+            *ProductPageLocators.ADDED_TO_CART_PRODUCT
+        ).text
+        assert product_name_header == product_name_message, (
+            'Product name is not same in \"added to cart\" message'
+        )
+
+    def should_be_same_cart_and_product_price(self):
+        product_price_on_page = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        product_price_in_message = self.browser.find_element(
+            *ProductPageLocators.CART_TOTAL_MESSAGE_PRICE
+        ).text
+        assert product_price_on_page == product_price_in_message, (
+            'Product price is not same in \"cart total\" message'
+        )
