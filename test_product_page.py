dbfcb2482778a3a6b7722b9d44ac1e1521aab029
Alexander Rybin <alex_rybin@hotmail.com>
2019-09-11 21:12:24 +0300

    Move login link functions to BaseClass Add login page tests to test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,4 @@
+from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 import pytest
 
@@ -27,35 +28,42 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_same_cart_and_product_price()
 
 
-@pytest.mark.parametrize(
-    'link',
-    ['http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/']
-)
 @pytest.mark.xfail
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart_click()
     page.should_not_be_success_message()
 
 
-@pytest.mark.parametrize(
-    'link',
-    ['http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/']
-)
-def test_guest_cant_see_success_message(browser, link):
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
 
 
-@pytest.mark.parametrize(
-    'link',
-    ['http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/']
-)
 @pytest.mark.xfail
-def test_message_disappeared_after_adding_product_to_basket(browser, link):
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart_click()
     page.success_message_disappeared()
+    
+    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
